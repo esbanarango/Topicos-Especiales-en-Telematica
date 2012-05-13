b8904f0c340969f771bed2136b130aa72ad5db2a
Esteba Arango <marranoparael31@gmail.com>
2012-05-13 17:41:48 -0500

    No need to singin after singup

--- Reto 4/Gossip/app/controllers/users_controller.rb
+++ Reto 4/Gossip/app/controllers/users_controller.rb
@@ -44,6 +44,8 @@ class UsersController < ApplicationController
   def create
     respond_to do |format|
       if @user.save
+
+        session[:user_id] = @user.id
         format.html { redirect_to @user, notice: "Welcome to Gossip!" }
         format.json { render json: @user, status: :created, location: @user }
       else
